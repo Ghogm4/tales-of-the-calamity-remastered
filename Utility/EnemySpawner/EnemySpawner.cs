2ecee7e67ad55e8245e592d69c5d331ca2d8ddba
Ghogm4 <30169665+Ghogm4@users.noreply.github.com>
2025-07-11 11:10:18 +0800

    Worked on EnemySpawner

--- Utility/EnemySpawner/EnemySpawner.cs
+++ Utility/EnemySpawner/EnemySpawner.cs
@@ -3,11 +3,39 @@ using System;
 
 public partial class EnemySpawner : Marker2D
 {
+	[Export] public string[] EnemyList = [];
+	[Export] public MovementRes[] MoveRes = [];
+	[Export] public float InitialWait = 1f;
+	[Export] public float SpawnInterval = 0.5f;
+	private float _spawnInterval = 0f;
+	private bool _duringInitialWait = true;
 	public override void _Ready()
 	{
 	}
 
-	public override void _Process(double delta)
+	public override void _PhysicsProcess(double delta)
 	{
+		InitialTimer(delta);
+		SpawnTimer(delta);
+	}
+	public void Spawn()
+	{
+
+	}
+	public void InitialTimer(double delta)
+	{
+		if (!_duringInitialWait) return;
+		InitialWait -= (float)delta;
+		if (InitialWait < 0) _duringInitialWait = false;
+	}
+	public void SpawnTimer(double delta)
+	{
+		if (_duringInitialWait) return;
+		_spawnInterval -= (float)delta;
+		if (_spawnInterval < 0)
+		{
+			_spawnInterval = SpawnInterval;
+			Spawn();
+		}
 	}
 }
